2e30ad668281d0ca176d57e337de0650a9268d3e
Nathan Kaique <119627814+nathankaique@users.noreply.github.com>
2023-01-27 19:16:32 +0000

    completando catalogo completo

---
+++ vassouras.js
@@ -0,0 +1,74 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'vassoura noviça',
+        img: 'images/vassouranoviça.jpg',
+        price: [13.00, 18.00, 24.00],
+        sizes: [
+          '1.40cm',
+          '1.51cm',
+          '1.63cm'
+        ],
+        description: 'Ela tem as cerdas pequenas com pontas suaves, que são perfeitas para pisos secos e delicados, pois evitam arranhões indesejáveis'
+      },
+      {
+        id: 2,
+        name: 'vassoura de Palha',
+        img: 'images/VassouraPalha.png',
+        price: [17.00, 19.50, 23.00],
+        sizes: [
+          '1.35cm',
+          '1.43cm',
+          '1.60cm'
+        ],
+        description: 'este é um modelo ideal para quem está procurando uma vassoura para limpar pátios e pisos externos, eliminando a sujeira que se acumula nestes locais'
+      },
+      {
+        id: 3,
+        name: 'Vassoura Piaçava',
+        img: 'images/VassouraPiaçava.jpg',
+        price: [17.75, 19.50, 25.00],
+        sizes: [
+          '1.45cm',
+          '1.53cm',
+          '1.65cm'
+        ],
+        description: 'elas são mais espessas e oferecem maior resistência em pisos rústicos, sem perder a sua boa durabilidade, que é um diferencial'
+      },
+      {
+        id: 4,
+        name: 'Vassoura Varry',
+        img: 'images/VassouraVarryCasa.jpg',
+        price: [10.00, 16.00, 20.00],
+        sizes: [
+          '1.35cm',
+          '1.42cm',
+          '1.50cm'
+        ],
+        description: 'Essa vassoura tem como um dos diferenciais uma escova larga, que conta com mais cerdas. Além disso, as fibras são feitas de PET reciclado, que é uma resina mais firme e funcional do que as cerdas naturais'
+      },
+      {
+        id: 5,
+        name: 'Vassoura Phenix',
+        img: 'images/VassouraPhenix.jpg',
+        price: [13.20, 15.50, 18.75],
+        sizes: [
+          '1.30cm',
+          '1.35cm',
+          '1.58cm'
+        ],
+        description: 'Quando se fala em vassoura Santa Maria, temos uma opção que está nos lares brasileiros há mais de 60 anos. Esse modelo conta com cabo de metal, que tem alta flexibilidade. Além disso, as cerdas são de plástico, então funcionam como um curinga para a limpeza'
+      },
+      {
+        id: 6,
+        name: 'Vassoura Sweeper MaxClean',
+        img: 'images/VassouraSweeperMaxClean.jfif',
+        price: [24.00, 26.00, 30.00],
+        sizes: [
+          '1.53cm',
+          '1.60cm',
+          '1.63cm'
+        ],
+        description: 'MSeu uso é indicado para superfícies lisas, como os pisos de madeira, mármore ou de borracha. É uma vassoura mágica com 2 escovas giratórias e outra central com rotação 360º que capturam sujeiras leves como o pó'
+      },
+];
